6476fcfaf830b5ba2278b99ec917c448ae20d83f
Ballazi <balram17dec1999@gmail.com>
2021-12-30 22:19:16 +0530

    forth

--- src/Components/Home Section/Latest Articles/LatestArticles.js
+++ src/Components/Home Section/Latest Articles/LatestArticles.js
@@ -1,6 +1,5 @@
 import "./LatestArticles.css";
-import React, { useState, useEffect } from "react";
-// import ArrayOfBlogs from "../../ArrayOfBlogs/ArrayOfBlogs";
+import React, { useState, useEffect ,useRef} from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
@@ -52,26 +51,65 @@ let LatestArticles = () => {
   const [postNumber, setpostNumber] = useState(6);
   const [blogs, setBlogs] = useState([]);
   const [toppost, setToppost] = useState([]);
-  const [slidpost, setSlidpost] = useState([]);
+  const ref = useRef([
+    {
+        "id": 0,
+        "author": "Balram Kumar",
+        "title": "Karan Johar, R Madhavan declare Ranveer Singh's 83 a blockbuster.",
+        "imgURL": "https://images.hindustantimes.com/img/2021/11/30/550x309/83_1638255666040_1638255693527.jpg",
+        "desc": "The trailer of Ranveer Singh-starrer sports drama 83 is finally out and has impressed viewers as well as people from the film industry. Dia Mirza and R Madhavan said they got goosebumps on watching the trailer while Karan Johar called it a ‘bonafide blockbuster’. R Madhavan appreciated the trailer and also praised Ranveer and Jiiva. He wrote on Twitter, “THIS ONE IS A MASSS BLOCKBUSTER-My Bro @RanveerOfficial and @JiivaOfficial are bloody brilliant. Fantasticccc.” Karan Johar tweeted, “Humungous congratulations to the entire cast and crew!!! This gave me goosebumps and is so emotional and arousing! BONAFIDE BLOCKBUSTER! Kabir!!! You’re the man and Ranveer you just became Kapil Dev with the ease of a veteran! Badhai ho (Congratulations)!”",
+        "category": "Bollywood",
+        "time": " /November 30 2021",
+        "tags": [
+            "Ranveer Singh",
+            "R Madhavan"
+        ],
+        "readtime": "2 mins read"
+    },
+    {
+        "id": 1,
+        "author": "Shawn Levy",
+        "imgURL": "https://nerdist.com/wp-content/uploads/2020/09/Free-Guy-Banner-Poster.jpeg",
+        "title": "Free Guy",
+        "desc": "When a bank teller discovers he's actually a background player in an open-world video game, he decides to become the hero of his own story -- one that he can rewrite himself. In a world where there's no limits, he's determined to save the day his way before it's too late, and maybe find a little romance with the coder who conceived him.",
+        "category": "Hollywood",
+        "time": " /August 13 2021",
+        "tags": [
+            "Action",
+            "Adventure"
+        ],
+        "readtime": "10 mins read"
+    },
+    {
+        "id": 2,
+        "author": "Jaume Collet-Serra",
+        "imgURL": "https://cdn.mos.cms.futurecdn.net/2BYffYyvNPf5fa9Hmudoum.jpg",
+        "title": "Jungle Cruise",
+        "desc": "Dr. Lily Houghton enlists the aid of wisecracking skipper Frank Wolff to take her down the Amazon in his ramshackle boat. Together, they search for an ancient tree that holds the power to heal -- a discovery that will change the future of medicine.",
+        "category": "Hollywood",
+        "time": " /july 30 2021",
+        "tags": [
+            "Action",
+            "Avengers"
+        ],
+        "readtime": "5 mins read"
+    }
+]
+)
+
 
   useEffect(() => {
     const url = "http://node-backend-react-blogs.herokuapp.com/api/v1/home/latestarticles";
     axios
       .get(url, { params: { postNumber: postNumber } })
-      .then((res) => {
+      .then(async (res) => {
         setBlogs(res.data.arr);
         setToppost(res.data.arr1);
-        setSlidpost(res.data.arr2);
       })
       .catch((err) => {
         console.log(JSON.stringify(err));
       });
-  });
-
-  // let {id1,imgURL1,title1,category1,time1} = slidpost[0];
-  // let {id2,imgURL2,title2,category2,time2} = slidpost[1];
-  // let {id3,imgURL3,title3,category3,time3} = slidpost[2];
-
+  }, [postNumber]);
 
   return (
     <div className="LatestArticles-Container">
@@ -85,10 +123,7 @@ let LatestArticles = () => {
           >
             <i className="fas fa-arrow-down"></i> Load More
           </div>
-
-            {/* {console.log(slidpost)};     */}
-
-          {/* <div
+          <div
             id="carouselExampleCaptions"
             className="carousel slide margtb2rem"
             data-bs-ride="carousel"
@@ -119,54 +154,54 @@ let LatestArticles = () => {
               <div className="carousel-item active">
                 <img
                   className="low-brigtness radnom5gh415f d-block w-100"
-                  src={slidpost[0].imgURL}
+                  src={ref.current[0].imgURL}
                   alt="cover"
                 />
                 <div className="carousel-caption d-md-block">
                   <Link
-                    to={`/${slidpost[0].category}/${slidpost[0].id}`}
+                    to={`/${ref.current[0].category}/${ref.current[0].id}`}
                   >
                     {" "}
-                    <h5>{slidpost[0].title}</h5>
+                    <h5>{ref.current[0].title}</h5>
                   </Link>
                   <p>
-                    {slidpost[0].category} {slidpost[0].time}
+                    {ref.current[0].category} {ref.current[0].time}
                   </p>
                 </div>
               </div>
               <div className="carousel-item ">
                 <img
                   className="low-brigtness radnom54fgh15f d-block w-100"
-                  src={slidpost[1].imgURL}
+                  src={ref.current[1].imgURL}
                   alt="sec"
                 />
                 <div className="carousel-caption d-md-block">
                   <Link
-                    to={`/${slidpost[1].category}/${slidpost[1].id}`}
+                    to={`/${ref.current[1].category}/${ref.current[1].id}`}
                   >
                     {" "}
-                    <h5>{slidpost[1].title}</h5>
+                    <h5>{ref.current[1].title}</h5>
                   </Link>
                   <p>
-                    {slidpost[1].category} {slidpost[1].time}
+                    {ref.current[1].category} {ref.current[1].time}
                   </p>
                 </div>
               </div>
               <div className="carousel-item ">
                 <img
                   className="low-brigtness radnom541nhjk5f d-block w-100"
-                  src={slidpost[2].imgURL}
+                  src={ref.current[2].imgURL}
                   alt="s"
                 />
                 <div className="carousel-caption d-md-block">
                   <Link
-                    to={`/${slidpost[2].category}/${slidpost[2].id}`}
+                    to={`/${ref.current[2].category}/${ref.current[2].id}`}
                   >
                     {" "}
-                    <h5>{slidpost[2].title}</h5>
+                    <h5>{ref.current[2].title}</h5>
                   </Link>
                   <p>
-                    {slidpost[2].category} {slidpost[2].time}
+                    {ref.current[2].category} {ref.current[2].time}
                   </p>
                 </div>
               </div>
@@ -200,25 +235,24 @@ let LatestArticles = () => {
         <div className="articles-right-home">
           <div className="advertisement-box-blog">Advertisement</div>
 
-          <br /> */}
+          <br />
           <div className="heading-home-latest border-btm">Top Posts</div>
-
           <div className="top-post-home margtb2rem">
-            {/* <img src={slidpost[0].imgURL} alt="kuch bhi" />
+            <img src={ref.current[0].imgURL} alt="kuch bhi" />
 
-            <Link to={`/${slidpost[0].category}/${slidpost[0].id}`}>
+            <Link to={`/${ref.current[0].category}/${ref.current[0].id}`}>
               {" "}
               <div className="latest-post-title-1 margtop1rem">
-                {slidpost[0].title}
+                {ref.current[0].title}
               </div>
             </Link>
 
             <div className="margtop1rem">
               <span className="latest-home-type ">
-                {slidpost[0].category}
+                {ref.current[0].category}
               </span>
-              <span className="latest-home-date">{slidpost[0].time}</span>
-            </div> */}
+              <span className="latest-home-date">{ref.current[0].time}</span>
+            </div>
 
             {toppost.map(createTopPOst)}
           </div>
